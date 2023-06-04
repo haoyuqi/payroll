56023bde7bcac6cdb7aeef27bdf2809821c17a07
Hao Yuqi <hi.haoyuqi+github@gmail.com>
2023-06-04 17:53:30 +0800

    fix soft delete

---
+++ database/migrations/2023_06_04_091540_add_soft_delete_for_all_tables.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $tables = ['departments', 'employees', 'paychecks', 'time_logs'];
+
+        try {
+            foreach ($tables as $table) {
+                Schema::table($table, function (Blueprint $table) {
+                    $table->softDeletes();
+                });
+            }
+        } catch (\Exception $e) {
+            throw new LogicException($e->getMessage());
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $tables = ['departments', 'employees', 'paychecks', 'time_logs'];
+
+        try {
+            foreach ($tables as $table) {
+                Schema::table($table, function (Blueprint $table) {
+                    $table->dropSoftDeletes();
+                });
+            }
+        } catch (\Exception $e) {
+            throw new LogicException($e->getMessage());
+        }
+    }
+};
